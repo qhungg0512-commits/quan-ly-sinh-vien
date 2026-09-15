e1c51d8808c2af7a39d8e4203fa1ba044a521b66
qhungg0512 <qhungg0512@gmail.com>
2026-09-16 01:37:53 +0700

    Upload project

---
+++ src/main/java/quanlysinhvien/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package quanlysinhvien.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/quanlysinhvien/demo/repository/StudentRepository.java
@@ -0,0 +1,19 @@
+package quanlysinhvien.demo.repository;
+import java.util.List;
+import java.util.UUID;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import quanlysinhvien.demo.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, UUID> {
+
+
+    List<Student> findByStudentCodeContainingIgnoreCaseOrFullNameContainingIgnoreCaseOrEmailContainingIgnoreCaseOrPhoneContainingIgnoreCase(
+            String studentCode,
+            String fullName,
+            String email,
+            String phone);
+}
+
